81bc4bdaac74a487c4d2ee626e3500558bd95184
Marlene Audic <rondineau.m@gmail.com>
2024-05-18 15:01:58 +0200

    Ajout du fichier sass

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
     add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
     function theme_enqueue_styles() {
         // Enqueue theme style
-        wp_enqueue_style('theme-style', get_template_directory_uri() . '/theme.css', array(), 
-        filemtime(get_stylesheet_directory() . '/theme.css'), 'all');
+        wp_enqueue_style('theme-style', get_template_directory_uri() . '/sass/theme.css', array(), 
+        filemtime(get_stylesheet_directory() . '/sass/theme.css'), 'all');
     }
 
 
