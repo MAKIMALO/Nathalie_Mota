e328b23c3351f2b423ce6e9e01e2501b8e67ddce
Marlene Audic <rondineau.m@gmail.com>
2024-05-22 12:04:03 +0200

    Etape 1 : création de la modale de contact + responsive

--- js/modale.js
+++ js/modale.js
@@ -1,25 +1,35 @@
 console.log("le fichier modale.js fonctionne");
 
 document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('contactModal');
-    var links = document.querySelectorAll('.contactLink');
-  
-    // Ouvrir la modale
-    links.forEach(function(link) {
-      link.onclick = function(event) {
-        event.preventDefault();
-        modal.classList.add('open');
-      }
-    });
-  
-    // Fermer la modale
-    function closeModal() {
-        modal.classList.remove('open');
-    }
+  var modal = document.getElementById('contactModal');
+  var links = document.querySelectorAll('.contactLink');
+  var closeBtn = document.querySelector('.closeBtn');
+  var body = document.querySelector('body');
 
-    window.onclick = function(event) {
-        if (event.target == modal ) {
-          modal.style.display = "none";
-        }
-    }
+  // Ouvrir la modale
+  links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          event.stopPropagation(); // Empêche la propagation de l'événement de clic
+          modal.classList.add('open');
+      });
   });
+
+  // Fermer la modale via la croix
+  closeBtn.addEventListener('click', function(event) {
+      event.stopPropagation(); // Empêche la propagation de l'événement de clic
+      closeModal();
+  });
+
+  // Fermer la modale en cliquant en dehors de la modale
+  body.addEventListener('click', function(event) {
+      if (modal.classList.contains('open') && !modal.contains(event.target)) {
+          closeModal();
+      }
+  });
+
+  // Fonction pour fermer la modale
+  function closeModal() {
+      modal.classList.remove('open');
+  }
+});
--- template-parts/contact_modale.php
+++ template-parts/contact_modale.php
@@ -2,7 +2,10 @@
 <!-- Ajout de la modale de contact -->
 
 <div id="contactModal" class="modale-overlay">
-    <img class="img_contact_modale" src="<?php echo get_template_directory_uri() . '/assets/images/Contact-header.webp'; ?>" alt="Titre de la modale de contact">
+    <div class="modale-header">
+        <span class="closeBtn">X</span>
+        <img class="img_contact_modale" src="<?php echo get_template_directory_uri() . '/assets/images/Contact-header.webp'; ?>" alt="Titre de la modale de contact">
+    </div>
     <div class="modale">
         <?php echo do_shortcode('[contact-form-7 id="87f570d" title="Modale contact"]');?>
     </div>
