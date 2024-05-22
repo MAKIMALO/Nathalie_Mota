700c028fc5833dab94149af99109b029401bd44d
Marlene Audic <rondineau.m@gmail.com>
2024-05-22 15:03:03 +0200

    Etape 1 : création de la modale de contact + responsive + fermeture à la croix ou au clic en dehors ok

--- template-parts/contact_modale.php
+++ template-parts/contact_modale.php
@@ -2,12 +2,14 @@
 <!-- Ajout de la modale de contact -->
 
 <div id="contactModal" class="modale-overlay">
-    <div class="modale-header">
-        <span class="closeBtn">X</span>
-        <img class="img_contact_modale" src="<?php echo get_template_directory_uri() . '/assets/images/Contact-header.webp'; ?>" alt="Titre de la modale de contact">
-    </div>
-    <div class="modale">
-        <?php echo do_shortcode('[contact-form-7 id="87f570d" title="Modale contact"]');?>
+    <div class="modale-global">    
+        <div class="modale-header">
+            <span class="closeBtn">X</span>
+            <img class="img_contact_modale" src="<?php echo get_template_directory_uri() . '/assets/images/Contact-header.webp'; ?>" alt="Titre de la modale de contact">
+        </div>
+        <div class="modale">
+            <?php echo do_shortcode('[contact-form-7 id="87f570d" title="Modale contact"]');?>
+        </div>
     </div>
 </div>
 
