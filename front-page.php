ca5b25121e351f529b638f3d4c7d1ec05a66e42b
Marlene Audic <rondineau.m@gmail.com>
2024-06-04 17:42:17 +0200

    Etape 4 : création de la page d'accueil avec l'importe des photos

--- front-page.php
+++ front-page.php
@@ -20,7 +20,8 @@
     </section>
 
     <!-- Affichage de l'ensemble des pages "photos" -->
-    <section class="photos-gallery">
+    <section class="section-gallery">
+        <div class="photos-gallery">
         <?php
             $per_page = 8;
             $offset = -1;
@@ -32,21 +33,20 @@
             );
             $query = new WP_Query( $args );
 
-            if ( $query->have_posts() ) :
-                while ( $query->have_posts() ) : $query->the_post(); ?>
-                    <div class="photo-item">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php if ( has_post_thumbnail() ) {
-                                the_post_thumbnail( 'medium' ); // Taille de l'image
-                            } ?>
-                        </a>
-                    </div>
-        <?php endwhile;
-        else : ?>
-            <p>No photos found</p>
-        <?php endif;
-        wp_reset_postdata(); ?>
-            <a href="<?php echo home_url( '?page=' . $offset + $per_page ); ?>">Charger plus</a>
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post();
+                    $image_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+                    $image_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+                    $post_id = get_the_ID();
+                    include(get_template_directory() . '/template-parts/photo_block.php');
+                endwhile;
+                wp_reset_postdata();
+            endif;
+            ?>
+        </div>
+        <div class="load-more-photos">
+            <button class="load_more" href="<?php echo home_url( '?page=' . $offset + $per_page ); ?>">Chargez plus</button>
+        </div>
     </section>
 
 </main>
